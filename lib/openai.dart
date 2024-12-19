d22dcd87272f4efa4ae532ee4212cd3c0524bd8f
ali <ali@DebianOs.myguest.virtualbox.org>
2024-12-19 17:01:16 +0200

    almost done

--- lib/openai.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-class OpenAiConfig {
-  static const String key =
-      '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';
-  static const String url = 'https://api.openai.com/v1/completions';
-}
