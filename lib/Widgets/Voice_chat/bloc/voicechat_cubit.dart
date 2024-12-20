9e1fce5c8cc49bf804109bb36f063598d41c07b7
ali <ali@DebianOs.myguest.virtualbox.org>
2024-12-20 21:12:52 +0200

    Need to send to server

--- lib/Widgets/Voice_chat/bloc/voicechat_cubit.dart
+++ lib/Widgets/Voice_chat/bloc/voicechat_cubit.dart
@@ -1,7 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class VoicechatCubit extends Cubit<bool> {
+  final _speechtotext = SpeechToText();
+  String recoSpeech = '';
+
   VoicechatCubit() : super(false);
-  void pressedButton() => emit(true);
-  void releasedButton() => emit(false);
+  void pressedButton() async {
+    emit(true);
+    bool available = await _speechtotext.initialize();
+    if (available) {
+      _speechtotext.listen(
+        onResult: (result) {
+          recoSpeech = result.recognizedWords;//TODO: Now I need to send this to the server.
+        },
+      );
+    }
+  }
+
+  void releasedButton() {
+    emit(false);
+    _speechtotext.stop();
+  }
 }
